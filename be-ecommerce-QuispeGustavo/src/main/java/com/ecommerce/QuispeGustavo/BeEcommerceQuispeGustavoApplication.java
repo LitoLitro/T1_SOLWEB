2bbbcff4ba6e29162adb0a6d07544b4d81fbc529
LitoLitro <gustavo.quispetr@gmail.com>
2026-09-23 20:26:15 -0500

    Commit N1

---
+++ be-ecommerce-QuispeGustavo/src/main/java/com/ecommerce/QuispeGustavo/BeEcommerceQuispeGustavoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.QuispeGustavo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeEcommerceQuispeGustavoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeEcommerceQuispeGustavoApplication.class, args);
+	}
+
+}
---
+++ be-ecommerce-QuispeGustavo/src/test/java/com/ecommerce/QuispeGustavo/BeEcommerceQuispeGustavoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.QuispeGustavo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeEcommerceQuispeGustavoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
