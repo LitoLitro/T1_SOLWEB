80f1b3826a99210d40034039b6811318f2a39cb9
LitoLitro <gustavo.quispetr@gmail.com>
2026-09-23 22:07:28 -0500

    Cuarto Commit t1

---
+++ be-ecommerce-QuispeGustavo/src/main/java/com/ecommerce/QuispeGustavo/dto/AuthResponseDTO.java
@@ -0,0 +1,13 @@
+package com.ecommerce.QuispeGustavo.dto;
+
+public class AuthResponseDTO {
+    private String token;
+    private String type = "Bearer";
+
+    public AuthResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() { return token; }
+    public String getType() { return type; }
+}
---
+++ be-ecommerce-QuispeGustavo/src/main/java/com/ecommerce/QuispeGustavo/entity/Rol.java
@@ -0,0 +1,26 @@
+package com.ecommerce.QuispeGustavo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String nombre; // Ejemplo: "ROLE_ADMIN", "ROLE_USER"
+
+    public Rol() {}
+
+    public Rol(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+}
